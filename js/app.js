90f45f4c1ec87fb64ca1716e3e373e256cbb6ae9
cdannyscela <cdannyscela@gmail.com>
2025-10-20 10:36:17 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ new Vue({
   data: {
     products: [
       { name: 'Audífono', price: 30000, stock: 3, image: 'https://www.dimarsa.cl/media/catalog/product/m/a/marcasphilips27phlt101b-negro1jpeg_0.jpg' },
-      { name: 'Mouse', price: 20000, stock: 5, image: 'https://via.placeholder.com/50' },
-      { name: 'Teclado', price: 15000, stock: 10, image: 'https://via.placeholder.com/50' },
-      { name: 'Gabinete', price: 35000, stock: 4, image: 'https://via.placeholder.com/50' },
-      { name: 'Pantalla', price: 175000, stock: 3, image: 'https://via.placeholder.com/50' },
-      { name: 'Silla', price: 150000, stock: 2, image: 'https://via.placeholder.com/50' }
+      { name: 'Mouse', price: 20000, stock: 5, image: 'https://media.spdigital.cl/thumbnails/products/5rq6eedr_aa395897_thumbnail_512.png' },
+      { name: 'Teclado', price: 15000, stock: 10, image: 'https://http2.mlstatic.com/D_NQ_NP_690994-MLA74651892230_022024-O.webp' },
+      { name: 'Gabinete', price: 35000, stock: 4, image: 'https://media.spdigital.cl/thumbnails/products/uarpex6c_d2479a00_thumbnail_512.jpg' },
+      { name: 'Pantalla', price: 175000, stock: 3, image: 'https://image.made-in-china.com/2f0j00WVGUZNfMEikm/18-5-Inch-Ultra-Slim-Desktop-Computer-PC-LED-Display-Screen-for-Gaming-Monitor.webp' },
+      { name: 'Silla', price: 150000, stock: 2, image: 'https://www.winpy.cl/files/39428-9061-Silla-Gamer-Cougar-HotRod-1.jpg' }
     ],
     cart: []
   },
